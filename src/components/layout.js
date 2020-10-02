9b5670eb2088abcdb4c46d7525e5595688890110
Thanh Viet Nguyen ( I532 ) <thanhvietnguyen@kms-technology.com>
2020-10-02 16:12:28 +0700

    Add html lang.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "./layouts/header"
 import Footer from "./layouts/footer"
+import { Helmet } from "react-helmet"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
+      <Helmet>
+        <html lang="en" />
+      </Helmet>
+
       <main>{children}</main>
       <Footer />
     </>
